a5bf2829ae24d09b81390f331a3d25309eaf89fe
Nada Ashraf <nada.ashraf.1.2.3.4.9@gmail.com>
2026-08-04 12:17:57 +0300

    add dispose method,update splash navigation and add app launcher icons

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:bmi_app/features/home_screen/presentation/home_screen_view.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -30,29 +29,32 @@ class _SplashViewBodyState extends State<SplashViewBody>
   }
 
   @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FadeTransition(
-            opacity: fadingAnimation!,
-            child: Center(
-              child: Image.asset(
-                "images/logo.png",
-                width: 115,
-                height: 115,
-              ),
-            ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FadeTransition(
+          opacity: fadingAnimation!,
+          child: Center(
+            child: Image.asset("images/logo.png", width: 115, height: 115),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
   void goToNextView() {
     Future.delayed(const Duration(seconds: 5), () {
-      Get.to(() => const HomeScreenView(), transition: Transition.fade);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreenView()),
+      );
     });
   }
 }
